02b9eb3ef3c4b6107fa08cedba372aefa39dce36
Teo Merida rodriguez <tmerida-@cbr15s6.42barcelona.com>
2022-05-31 14:12:32 +0200

    hola

--- src/print_message.c
+++ src/print_message.c
@@ -15,7 +15,7 @@
 void	print_message(t_philos *philos, char *str)
 {
 	pthread_mutex_lock(&philos->data->message);
-	if (philos->data->dead == 0)
+	if (philos->data->dead == 0 && philos->num_eats != 0)
 		printf("[%lld] philo [%d] %s\n",
 			(get_time() - philos->data->init_time), philos->id, str);
 	pthread_mutex_unlock(&philos->data->message);
